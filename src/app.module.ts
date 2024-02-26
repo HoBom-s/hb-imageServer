291f4ef4dbfd9c3649fb04f304e4002b532df6de
robinyeon <robin.yeon@gmail.com>
2024-02-26 23:19:00 +0900

    fix: add domain folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ImageController } from './image/image.controller';
-import { ImageService } from './image/image.service';
+import { ImageController } from './domain/image/image.controller';
+import { ImageService } from './domain/image/image.service';
 
 @Module({
   imports: [],
