a7cd6fd015917f98e798a7538fed1d1dd34f8ef7
robinyeon <robin.yeon@gmail.com>
2024-03-02 15:41:24 +0900

    fix: upgrade version (aws-sdk v2 to v3)

--- src/domain/images/images.controller.ts
+++ src/domain/images/images.controller.ts
@@ -6,7 +6,8 @@ import {
 } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import * as AWS from 'aws-sdk';
+// import * as AWS from 'aws-sdk';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 @Controller('images')
 export class ImagesController {
@@ -15,26 +16,50 @@ export class ImagesController {
   @Post()
   @UseInterceptors(FilesInterceptor('thumbnail'))
   async uploadImages(@UploadedFiles() files: Express.Multer.File[]) {
+    const client = new S3Client({
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      },
+      region: process.env.S3_REGION,
+    });
+
+    const key = `${Date.now().toString()}-${files[0].originalname}`;
+
+    const command = new PutObjectCommand({
+      Key: key,
+      Body: files[0].buffer,
+      Bucket: process.env.S3_BUCKET_NAME,
+    });
+
+    try {
+      const response = await client.send(command);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
+
+    /*
     AWS.config.update({
       credentials: {
         accessKeyId: process.env.ACCESS_KEY_ID,
         secretAccessKey: process.env.SECRET_ACCESS_KEY,
       },
-      region: 'ap-northeast-2',
+      region: process.env.S3_REGION,
     });
 
-    const key = `${Date.now()}-${files[0].originalname}`;
+  
+    const s3 = new AWS.S3();
 
     try {
-      await new AWS.S3()
-        .putObject({
-          Key: key,
-          Body: files[0].buffer,
-          Bucket: process.env.BUCKET_NAME,
-        })
-        .promise();
+      s3.putObject({
+        Key: key,
+        Body: files[0].buffer,
+        Bucket: process.env.S3_BUCKET_NAME,
+      }).promise();
     } catch (error) {
       console.error(error);
     }
+    */
   }
 }
