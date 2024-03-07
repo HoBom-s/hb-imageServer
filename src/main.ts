cb12f4c8f5f6247cc91b6f90d4e9fa774ef92115
robinyeon <robin.yeon@gmail.com>
2024-03-07 23:01:56 +0900

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   const PORT = parseInt(process.env.PORT, 10) || 3000;
-
+  app.enableCors({
+    origin: process.env.HBTB_V2,
+  });
   app.use(json({ limit: '50mb' }));
   await app.listen(PORT);
 }
