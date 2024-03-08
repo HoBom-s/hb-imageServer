bd05b009990fc8124b813f72c9f4359b593e197b
robinyeon <robin.yeon@gmail.com>
2024-03-08 09:28:52 +0900

    test: test for connection check

--- src/domain/images/images.controller.ts
+++ src/domain/images/images.controller.ts
@@ -1,10 +1,21 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesBodyData } from 'src/types/image.type';
 
 @Controller('images')
 export class ImagesController {
   constructor(private imagesService: ImagesService) {}
+
+  @Get()
+  test() {
+    return 'test';
+  }
 
   @Post()
   async uploadImages(@Body() images: ImagesBodyData) {
