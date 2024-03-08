25670e7e3f4488268bd3e083dc09fb4f9dc6d2ef
robinyeon <robin.yeon@gmail.com>
2024-03-08 10:31:38 +0900

    fix: corsOptions

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import { config } from 'dotenv';
 
 config();
 
+const corsOptions = {
+  origin: process.env.HBTB_V2,
+  allowedHeaders: 'Content-Type',
+  credentials: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   const PORT = parseInt(process.env.PORT, 10) || 3000;
-  app.enableCors({
-    origin: process.env.HBTB_V2,
-  });
+  app.enableCors(corsOptions);
   app.use(json({ limit: '50mb' }));
   await app.listen(PORT);
 }
