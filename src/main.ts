4b8d839f3992e6c30ec125f888ef646cf318678a
robinyeon <robin.yeon@gmail.com>
2024-03-08 11:39:36 +0900

    feat: winston logger

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json } from 'express';
 import { config } from 'dotenv';
+import { winstonLogger } from './utils/winston.util';
 
 config();
 
 const corsOptions = {
   origin: process.env.HBTB_V2,
-  allowedHeaders: 'Content-Type',
-  credentials: true,
 };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+    abortOnError: false,
+  });
   const PORT = parseInt(process.env.PORT, 10) || 3000;
+
   app.enableCors(corsOptions);
   app.use(json({ limit: '50mb' }));
+
   await app.listen(PORT);
 }
 bootstrap();
