8d27806ae60778b1f0d681bffc729e77761f198a
robinyeon <robin.yeon@gmail.com>
2024-03-09 19:14:25 +0900

    fix: winston level

--- src/utils/winston.util.ts
+++ src/utils/winston.util.ts
@@ -2,27 +2,7 @@ import { utilities, WinstonModule } from 'nest-winston';
 import * as winstonDaily from 'winston-daily-rotate-file';
 import * as winston from 'winston';
 
-const logDir = __dirname + '/../../logs'; // log 파일을 관리할 폴더
-console.log(logDir);
-
-export const winstonLogger = WinstonModule.createLogger({
-  transports: [
-    new winston.transports.Console({
-      level: 'silly',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        utilities.format.nestLike('HoBom-ImageServer', {
-          prettyPrint: true,
-          colors: true,
-        }),
-      ),
-    }),
-
-    new winstonDaily(dailyOptions('info')),
-    new winstonDaily(dailyOptions('warn')),
-    new winstonDaily(dailyOptions('error')),
-  ],
-});
+const logDir = __dirname + '/../../logs';
 
 function dailyOptions(level: string) {
   return {
@@ -34,3 +14,19 @@ function dailyOptions(level: string) {
     zippedArchive: true,
   };
 }
+
+export const winstonLogger = WinstonModule.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        utilities.format.nestLike('HoBom-ImageServer', {
+          prettyPrint: true,
+          colors: true,
+        }),
+      ),
+    }),
+
+    new winstonDaily(dailyOptions('info')),
+  ],
+});
