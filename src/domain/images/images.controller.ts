57ea91df9c8bff1bbd1b105c01770a0561d65c7d
robinyeon <robin.yeon@gmail.com>
2024-03-14 17:37:24 +0900

    fix: user profile upload

--- src/domain/images/images.controller.ts
+++ src/domain/images/images.controller.ts
@@ -13,18 +13,17 @@ export class ImagesController {
         'Error(ImageServer): Request body missing.',
       );
 
-    const { uniqueString, path, ext, buffer } = imageInfo;
+    let { uniqueString, path, ext, buffer } = imageInfo;
 
-    let uniqueStringWithoutSlash: string;
     if (path === 'thumbnail') {
       const isSlashIncluded = uniqueString.startsWith('/');
       if (isSlashIncluded) {
-        uniqueStringWithoutSlash = uniqueString.replace('/', '');
+        uniqueString = uniqueString.replace('/', '');
       }
     }
 
     return this.imagesService.uploadOneImage({
-      uniqueString: uniqueStringWithoutSlash,
+      uniqueString,
       path,
       ext,
       buffer,
